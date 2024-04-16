4221069d3a45c26d3f4ecadc78f23ea7b0a6d452
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2024-04-16 15:45:16 -0300

    add routes

--- lib/core/consts/app_routes.dart
+++ lib/core/consts/app_routes.dart
@@ -1,6 +1,4 @@
 class AppRoutes {
   static const String base = '/';
   static const String onboarding = '/onboarding';
-  static const String auth = '/auth';
-  static const String signUp = '/signUp';
 }

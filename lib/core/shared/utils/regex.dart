83317c2b3077e438e6a17930fed880fa53d30894
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2024-04-24 20:10:10 -0300

    add regex

---
+++ lib/core/shared/utils/regex.dart
@@ -0,0 +1,9 @@
+final RegExp emailPattern = RegExp(
+  r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+  r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a"
+  r"-zA-Z0-9])?)*$",
+  caseSensitive: false,
+  multiLine: false,
+);
+
+final RegExp passwordPattern = RegExp(r"^(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,}$");

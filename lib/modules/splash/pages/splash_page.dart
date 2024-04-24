fe933f3f6bd14665ccc40f599708dbbb0be3ee7c
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2024-04-24 20:10:59 -0300

    add popscope

--- lib/modules/splash/pages/splash_page.dart
+++ lib/modules/splash/pages/splash_page.dart
@@ -14,40 +14,43 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/background_splash.png"),
-                fit: BoxFit.fill,
-              ),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    AppColors.brown.withOpacity(.79),
-                    Colors.transparent,
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background_splash.png"),
+                  fit: BoxFit.fill,
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(height: height * 0.21),
-                  const LogoAnimatedWidget(),
-                  const Spacer(),
-                  const ButtonAnimatedWidget(),
-                  SizedBox(height: height * 0.08),
-                ],
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      AppColors.brown.withOpacity(.79),
+                      Colors.transparent,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: height * 0.21),
+                    const LogoAnimatedWidget(),
+                    const Spacer(),
+                    const ButtonAnimatedWidget(),
+                    SizedBox(height: height * 0.08),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

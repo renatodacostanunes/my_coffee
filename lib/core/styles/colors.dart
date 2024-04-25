deeb8813861cfdea314d3f01c486d53afc3c89ea
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2024-04-24 21:23:19 -0300

    add more colors

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -4,6 +4,8 @@ class AppColors {
   static const primary = Color(0XFFCE9760);
   static const background = Color(0XFF543A20);
   static const grey = Color(0XFFD9D9D9);
+  static const greyDark = Color(0XFF16181C);
+  static const greyLight = Color(0XFF828282);
   static const white = Color(0XFFFFFFFF);
   static const brown = Color(0XFF543A20);
   static const transparent = Colors.transparent;

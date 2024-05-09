c0679f5ceef3005888dd1872a4978d2afc86d58a
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2024-05-08 21:40:15 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_coffee/app_module.dart';
 import 'package:my_coffee/app_widget.dart';
 
-void main() async => runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
